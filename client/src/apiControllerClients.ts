542c25a83d794e3f3124254b028ba9e323f93640
andersthomsen92 <doomknight92@gmail.com>
2025-05-06 15:58:03 +0200

    Changed vite url to ip in workflow, reverted api controller to api, reverted apicontrollerclient to original

--- client/src/apiControllerClients.ts
+++ client/src/apiControllerClients.ts
@@ -1,16 +1,11 @@
 import {AuthClient, SubscriptionClient, WeatherStationClient} from "./generated-client.ts";
 
 const baseUrl = import.meta.env.VITE_API_BASE_URL
+
 const prod = import.meta.env.PROD
 
-const isRelativePath = baseUrl.startsWith('/');
+export const subscriptionClient = new SubscriptionClient(prod ? "https://" + baseUrl : "http://" + baseUrl);
 
-export const subscriptionClient = new SubscriptionClient(
-    isRelativePath ? baseUrl : (prod ? "https://" + baseUrl : "http://" + baseUrl)
-);
-export const weatherStationClient = new WeatherStationClient(
-    isRelativePath ? baseUrl : (prod ? "https://" + baseUrl : "http://" + baseUrl)
-);
-export const authClient = new AuthClient(
-    isRelativePath ? baseUrl : (prod ? "https://" + baseUrl : "http://" + baseUrl)
-);
+export const weatherStationClient = new WeatherStationClient(prod ? "https://" + baseUrl : "http://" + baseUrl);
+
+export const authClient = new AuthClient(prod ? "https://" + baseUrl : "http://" + baseUrl);
