5a468d523efd7e5a207f60f7d032ea0026c38acd
andersthomsen92 <doomknight92@gmail.com>
2025-05-06 16:09:35 +0200

    testing relative path with ip still in workflow

--- client/src/apiControllerClients.ts
+++ client/src/apiControllerClients.ts
@@ -1,11 +1,16 @@
 import {AuthClient, SubscriptionClient, WeatherStationClient} from "./generated-client.ts";
 
 const baseUrl = import.meta.env.VITE_API_BASE_URL
-
 const prod = import.meta.env.PROD
 
-export const subscriptionClient = new SubscriptionClient(prod ? "https://" + baseUrl : "http://" + baseUrl);
+const isRelativePath = baseUrl.startsWith('/');
 
-export const weatherStationClient = new WeatherStationClient(prod ? "https://" + baseUrl : "http://" + baseUrl);
-
-export const authClient = new AuthClient(prod ? "https://" + baseUrl : "http://" + baseUrl);
+export const subscriptionClient = new SubscriptionClient(
+    isRelativePath ? baseUrl : (prod ? "https://" + baseUrl : "http://" + baseUrl)
+);
+export const weatherStationClient = new WeatherStationClient(
+    isRelativePath ? baseUrl : (prod ? "https://" + baseUrl : "http://" + baseUrl)
+);
+export const authClient = new AuthClient(
+    isRelativePath ? baseUrl : (prod ? "https://" + baseUrl : "http://" + baseUrl)
+);
