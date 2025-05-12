e958e0d52f49a1eb66c1870d25a71672f5378ab3
RoedGroed <jonasroer92@gmail.com>
2025-05-12 10:37:54 +0200

    why is it not working, test with meta.env.WS_URL

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,9 +3,9 @@ import { WsClientProvider } from 'ws-request-hook';
 import ApplicationRoutes from "./ApplicationRoutes.tsx";
 import { DevTools } from "jotai-devtools";
 import 'jotai-devtools/styles.css';
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'; 
 
-const wsUrl = import.meta.env.VITE_API_WS_URL
+const baseUrl = import.meta.env.WS_URL;
 const prod = import.meta.env.PROD;
 
 export const randomUid = uuidv4();
@@ -14,9 +14,10 @@ export default function App() {
     const [serverUrl, setServerUrl] = useState<string | undefined>(undefined);
 
     useEffect(() => {
-        const finalUrl = `ws://${wsUrl}?id=${randomUid}`;
+        const finalUrl = `ws://${baseUrl}?id=${randomUid}`;
+
         setServerUrl(finalUrl);
-    }, []);
+    }, [prod, baseUrl]);
 
     return (
         <>
