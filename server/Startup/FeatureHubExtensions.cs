4db1c7401539db4f4bd4b34912a8f2e2e26a40a2
andersthomsen92 <doomknight92@gmail.com>
2025-05-15 17:22:11 +0200

    Changed to featurehub refreshes via connection

--- server/Startup/FeatureHubExtensions.cs
+++ server/Startup/FeatureHubExtensions.cs
@@ -2,6 +2,8 @@
 using Application.Models;
 using FeatureHubSDK;
 
+
+
 public static class FeatureHubExtensions
 {
     public static IServiceCollection RegisterFeatureHub(this IServiceCollection services)
@@ -9,7 +11,12 @@ public static class FeatureHubExtensions
         services.AddSingleton<IFeatureHubRepository>(sp =>
         {
             var config = sp.GetRequiredService<IOptions<AppOptions>>().Value;
+
             var fhConfig = new EdgeFeatureHubConfig(config.FEATUREHUB_URL, config.FEATUREHUB_API_KEY);
+
+            // ✅ Start streaming connection (we don't use the result directly here)
+            fhConfig.NewContext().Build().GetAwaiter().GetResult();
+
             return fhConfig.Repository;
         });
 
