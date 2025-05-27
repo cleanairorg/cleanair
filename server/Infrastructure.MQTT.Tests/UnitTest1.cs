36e840239bf153c670a9300ca62a1287ac74f67b
andersthomsen92 <doomknight92@gmail.com>
2025-05-27 16:56:03 +0200

    MQTT Test Project

---
+++ server/Infrastructure.MQTT.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Infrastructure.MQTT.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
